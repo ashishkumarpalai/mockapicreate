56df390c541bb4afa5a370707dee7baa07050b6d
Ashish Kumar Palai <ashishkumarpalai2000@gmail.com>
2023-01-19 13:37:05 +0530

    addd

---
+++ frontend-files/scripts/todos.js
@@ -0,0 +1,30 @@
+const baseURL = "http://127.0.0.1:9090";
+const recipeIngredientURL = "http://127.0.0.1:9090/recipeIngredients";
+const employeeURL = "http://localhost:9090/employees";
+const todosURL = "http://localhost:9090/todos";
+
+let mainSection = document.getElementById("data-list-wrapper");
+let paginationWrapper = document.getElementById("pagination-wrapper");
+
+let registerUserUsername = document.getElementById("register-user-username");
+let registerUserFirstname = document.getElementById("register-user-firstname");
+let registerUserLastname = document.getElementById("register-user-lastname");
+let registerUserEmail = document.getElementById("register-user-email");
+let registerUserPassowrd = document.getElementById("register-user-passowrd");
+let registerUserAvatar = document.getElementById("register-user-avatar");
+let registerUserLevel = document.getElementById("register-user-level");
+let registerUserButton = document.getElementById("register-user");
+
+let updateUserUsername = document.getElementById("update-user-username");
+let updateUserFirstname = document.getElementById("update-user-firstname");
+let updateUserLastname = document.getElementById("update-user-lastname");
+let updateUserEmail = document.getElementById("update-user-email");
+let updateUserPassowrd = document.getElementById("update-user-passowrd");
+let updateUserAvatar = document.getElementById("update-user-avatar");
+let updateUserLevel = document.getElementById("update-user-level");
+let updateUserButton = document.getElementById("update-user");
+
+
+window.addEventListener("load", () => {
+
+});
